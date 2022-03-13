010eae048040106e4328b8ebb5f6f4c20309a7ac
alphatoaster <32408375+alphatoasterous@users.noreply.github.com>
2022-03-13 17:10:17 +0600

    Update xinit_user.sh
    Because detaching xinit was not a good idea.

--- xinit_user.sh
+++ xinit_user.sh
@@ -7,10 +7,9 @@ if [ $# -eq 0 ]
 fi
 
 if grep "$1" /etc/passwd >/dev/null 2>&1; then
-  su - $1 -c "startx -- :0 vt7" &
+  su - $1 -c "startx -- :0 vt7"
+  exit 0
 else
   echo "User not found"
   exit 2
 fi
-
-exit 0
